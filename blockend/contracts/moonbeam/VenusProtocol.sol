09d04fcb5d24c2cc2b0f9744804539ae60b60c0e
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-03-01 13:27:29 -0700

    add complete venus protocol and connector

--- blockend/contracts/moonbeam/VenusProtocol.sol
+++ blockend/contracts/moonbeam/VenusProtocol.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.12;
 
 import "../wormhole/QueryResponse.sol";
+import "../interfaces/IWormholeRelayer.sol";
+import "../interfaces/IWormholeReceiver.sol";
 
-contract VenusProtocol is QueryResponse {
+contract VenusProtocol is QueryResponse, IWormholeReceiver {
 
     enum OrderStatus {
         DOES_NOT_EXIST,
@@ -12,62 +14,128 @@ contract VenusProtocol is QueryResponse {
         FAILED
     }
 
+    enum Relayer {
+        WORMHOLE,
+        AXELAR
+    }
+
+    enum CrossChainResponse{
+        SUCCESS,
+        NOT_APPROVED, 
+        NOT_OWNED
+    }
+
     struct Listing {
         address seller;
         address tokenAddress;
         uint256 tokenId;
-        uint256 wormholeChainId;
+        uint256 chainId;
         uint256 priceInNative;
-        bytes signature;
+        uint256 validity;
         bool isActive;
     }
 
     struct Order {
         uint256 orderId;
         uint256 listingId;
-        uint256 wormholeChainId;
+        uint256 chainId;
         address buyer;
-        uint256 pricePaidInNative;
+        Relayer relayer;
         OrderStatus status;
     }
+    
+    struct CrossChainQueryData{
+        bytes response;
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        uint8 guardianIndex;
+    }
 
-
+    uint256 public constant GAS_LIMIT = 50_000;
     mapping(uint256 => Listing) public listings;
     mapping(uint256 => Order) public orders;
+    mapping(uint16 => address) public whitelistedWormholeAddresses;
+    mapping(string => string) public whitelistedAxelarAddresses;
+    mapping(uint256=>uint16) public chainIdsToWormholeChainIds;
+    mapping(uint256=>string) public chainIdsToAxelarChainNames;
+    mapping(address=>uint256) public claimables;
     uint256 public orderIdCounter;
     uint256 public listingIdCounter;
-    
-    event NFTListed(uint256 listingId, address seller, address tokenAddress, uint256 tokenId, uint256 priceInNative);
+    address public owner;
+    IWormholeRelayer public immutable wormholeRelayer;
+
+    event NFTListed(uint256 listingId, address seller, address tokenAddress, uint256 tokenId, uint256 chainId, uint256 validity, uint256 priceInNative);
     event NftPurchaseInitiated(uint256 orderId, uint256 foreignChainListingId, uint256 wormholeChainId, address buyer, uint256 pricePaidInNative);
-    event NftPurchaseCompleted(uint256 orderId, address seller);
-    event NftPurchaseFailed(uint256 orderId);
+    event NftPurchaseCompleted(uint256 orderId);
+    event NftPurchaseFailed(uint256 orderId, uint8 returnValue);
+    event CrossChainQueryLogger(ParsedQueryResponse r, EthCallQueryResponse eqr, address owner);
 
-
-    constructor(address _wormhole) QueryResponse(_wormhole) {
+    constructor(address _wormholeCoreAddress, address _wormholeRelayer) QueryResponse(_wormholeCoreAddress)  {
+        owner=msg.sender;
+        wormholeRelayer=IWormholeRelayer(_wormholeRelayer);
         orderIdCounter = 0;
         listingIdCounter = 0;
     }
 
-    function purchaseNFT(uint256 listingId, uint256 wormholeChainId) public payable  returns(uint256 orderId){
-        emit NftPurchaseInitiated(orderIdCounter, listingId, wormholeChainId, msg.sender, msg.value);
-        orderIdCounter++;
-        return 0;
+    modifier onlyOwner()
+    {
+        require(msg.sender==owner, "Only owner allowed");    
+        _;
     }
 
-    function listNft(address tokenAddress, uint256 tokenId, uint256 nativePrice, bytes memory response, IWormhole.Signature[] memory signatures) public {
-        // Wormhole Cross chain queries for fetching approval?
-        // require(verifyApprovalCrossChainQuery(response, signatures), "Approval not verified");
-        emit NFTListed(listingIdCounter, msg.sender, tokenAddress, tokenId, nativePrice);
-        listingIdCounter++;
-    }
-
-    function nftPurchaseCallback(uint256 orderId, address seller, bool isSuccess) public {
-        if(isSuccess)
-        {
-            emit NftPurchaseCompleted(orderId, seller);
-        }else{
-            emit NftPurchaseFailed(orderId);
+    function whitelistWormholeAddress(uint256[] memory chainIds, uint16[] memory _wormholeChainIds, address[] memory destinationAddresses) public onlyOwner{
+        for(uint i=0; i<_wormholeChainIds.length; i++){
+            whitelistedWormholeAddresses[_wormholeChainIds[i]] = destinationAddresses[i];
+            chainIdsToWormholeChainIds[chainIds[i]] = _wormholeChainIds[i];
         }
+    }
+
+    function whitelistAxelarAddresses(uint256[] memory chainIds, string[] memory chainNames, string[] memory destinationAddresses) public onlyOwner{
+        for(uint i=0; i<chainIds.length; i++){
+            chainIdsToAxelarChainNames[chainIds[i]] = destinationAddresses[i];
+            whitelistedAxelarAddresses[chainNames[i]] = destinationAddresses[i];
+        }
+    }
+
+    function purchaseNFTViaWormhole(uint256 listingId, uint256 receiverValue) public payable  returns(uint256 orderId){
+        uint16 wormholeChainId = chainIdsToWormholeChainIds[listings[listingId].chainId];
+        uint256 cost=quoteCrossChainCall(wormholeChainId, receiverValue);
+        
+        require(msg.value >= listings[listingId].priceInNative+cost, "Incorrect price paid"); // check correct price paid
+        require(listings[listingId].isActive, "Listing does not exist or inactive"); // check if listing is active or exists
+        require(whitelistedWormholeAddresses[wormholeChainId] != address(0), "Wormhole address not whitelisted"); // check if whitelisted wormhole address exists
+        
+        orders[orderIdCounter]=Order(orderIdCounter, listingId, listings[listingId].chainId, msg.sender, Relayer.WORMHOLE, OrderStatus.PENDING);
+        
+        wormholeRelayer.sendPayloadToEvm{value: cost}(
+            wormholeChainId,
+            whitelistedWormholeAddresses[wormholeChainId],
+            abi.encode(orderIdCounter, listings[listingId].tokenAddress,listings[listingId].tokenId, msg.sender, listings[listingId].seller), // payload
+            receiverValue, // no receiver value needed since we're just passing a message
+            GAS_LIMIT,
+            wormholeChainId,
+            owner
+        );
+        
+        emit NftPurchaseInitiated(orderIdCounter, listingId, wormholeChainId, msg.sender, msg.value);
+        
+        orderIdCounter++;
+        return orderIdCounter-1;
+    }
+
+    // function purchaseNFTViaAxelar(uint256 listingId) public payable returns(uint256 orderId)
+    // {
+
+    // }
+
+    function listNft(address tokenAddress, uint256 tokenId, uint256 nativePrice, uint256 validity, uint256 chainId, CrossChainQueryData calldata _crosschainQueryData) public {
+        // Wormhole Cross chain queries for fetching approval
+        // verify owner of the nft, verify approval
+        // require(verifyApprovalCrossChainQuery(response, signatures), "Approval not verified");
+        listings[listingIdCounter] = Listing(msg.sender, tokenAddress, tokenId, chainId, nativePrice, validity, true);
+        emit NFTListed(listingIdCounter, msg.sender,  tokenAddress,  tokenId,  chainId,  validity,  nativePrice);
+        listingIdCounter++;
     }
 
     function getOrderIdCounter() public view returns(uint256){
@@ -77,15 +145,52 @@ contract VenusProtocol is QueryResponse {
     function getListingIdCounte() public view returns(uint256){
         return listingIdCounter;
     }
-    event Logger(ParsedQueryResponse r, EthCallQueryResponse eqr, address owner);
-    function verifyApprovalCrossChainQuery(bytes memory response, bytes32 rSig, bytes32 sSig, uint8 vSig, uint8 guardianIndexSig) public  returns(bool){
+    function verifyApprovalCrossChainQuery(CrossChainQueryData calldata __crosschainQueryData) public returns(bool){
         IWormhole.Signature[] memory signatures = new IWormhole.Signature[](1);
-        signatures[0] = IWormhole.Signature(rSig, sSig, vSig, guardianIndexSig);
-        ParsedQueryResponse memory r = parseAndVerifyQueryResponse(response, signatures);
+        signatures[0] = IWormhole.Signature(__crosschainQueryData.r,__crosschainQueryData.s, __crosschainQueryData.v, __crosschainQueryData.guardianIndex);
+        ParsedQueryResponse memory r = parseAndVerifyQueryResponse(__crosschainQueryData.response, signatures);
         EthCallQueryResponse memory eqr = parseEthCallQueryResponse(r.responses[0]);
         address owner=abi.decode(eqr.result[0].result, (address));
 
-        emit Logger(r, eqr, owner);
+        emit CrossChainQueryLogger(r, eqr, owner);
         return owner == msg.sender;
+    }
+
+
+    function receiveWormholeMessages(
+        bytes memory payload,
+        bytes[] memory, // additionalVaas
+        bytes32 sourceAddressBytes32, // address that called 'sendPayloadToEvm' (HelloWormhole contract address)
+        uint16 sourceChain,
+        bytes32 // unique identifier of delivery
+    ) public payable override {
+        require(msg.sender == address(wormholeRelayer), "Only relayer allowed");
+        address sourceAddress=address(uint160(uint256(sourceAddressBytes32)));
+        require(whitelistedWormholeAddresses[sourceChain] == sourceAddress, "Address not whitelisted");
+        // Parse the payload and do the corresponding actions!
+        (uint256 orderId, uint8 returnValue) = abi.decode(
+            payload,
+            (uint256, uint8)
+        );
+        if(returnValue==0){
+            orders[orderId].status = OrderStatus.COMPLETED;
+            claimables[listings[orders[orderId].listingId].seller] += listings[orders[orderId].listingId].priceInNative;
+            emit NftPurchaseCompleted(orderId);
+        }else{
+            orders[orderId].status = OrderStatus.FAILED;
+            claimables[orders[orderId].buyer] += listings[orders[orderId].listingId].priceInNative;
+            emit NftPurchaseFailed(orderId, returnValue);
+        }
+    }
+
+
+    function quoteCrossChainCall(
+        uint16 targetChain, uint256 receiverValue
+    ) public view returns (uint256 cost) {
+        (cost, ) = wormholeRelayer.quoteEVMDeliveryPrice(
+            targetChain,
+            receiverValue,
+            GAS_LIMIT
+        );
     }
 }
--- blockend/contracts/mulit-chain/VenusConnector.sol
+++ blockend/contracts/mulit-chain/VenusConnector.sol
@@ -1,15 +1,67 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
+import "../interfaces/IWormholeRelayer.sol";
+import "../interfaces/IWormholeReceiver.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract VenusConnector {
+contract VenusConnector is IWormholeReceiver{
 
-    uint256 public listingIdCounter;
+    IWormholeRelayer public immutable wormholeRelayer;
+    address public immutable protocolAddress;
+    uint256 public immutable protocolChainId;
+    uint16 public immutable protocolWormholeChainId;
+
+    uint256 public constant GAS_LIMIT = 50_000;
+
+
+    constructor (address _wormholeRelayer, address _protocolAddress, uint256 _protocolChainId, uint16 _protocolWormholeChainId, string memory _protocolAxelarChainName) {
+        wormholeRelayer = IWormholeRelayer(_wormholeRelayer);
+        protocolAddress = _protocolAddress;
+        protocolChainId = _protocolChainId;
+        protocolWormholeChainId = _protocolWormholeChainId;
+    }
     
-    event NFTListed(uint256 listingId, address seller, address tokenAddress, uint256 tokenId, uint256 priceInNative);
+    function receiveWormholeMessages(
+        bytes memory payload,
+        bytes[] memory, // additionalVaas
+        bytes32 sourceAddressBytes32, // address that called 'sendPayloadToEvm' (HelloWormhole contract address)
+        uint16 sourceChain,
+        bytes32 // unique identifier of delivery
+    ) public payable override {
+        require(msg.sender == address(wormholeRelayer), "Only relayer allowed");
+        address sourceAddress=address(uint160(uint256(sourceAddressBytes32)));
+        require(protocolAddress == sourceAddress, "Address not allowed");
+        require(sourceChain == protocolWormholeChainId, "Chain not allowed");
+        require(msg.value>=quoteCrossChainCall(sourceChain, 0), "Insufficient cross chain fee");
+        // Parse the payload and do the corresponding actions!
+        (uint256 orderId, address tokenAddress, uint256 tokenId, address buyer, address seller) = abi.decode(
+            payload,
+            (uint256, address, uint256, address, address)
+        );
+        bool isOwner=IERC721(tokenAddress).ownerOf(tokenId) == seller;
+        bool isApproved=IERC721(tokenAddress).getApproved(tokenId) == address(this);
+        uint8 state= isOwner && isApproved ? 0 : isOwner ? 1 : 2;
+        wormholeRelayer.sendPayloadToEvm{value: msg.value}(
+            sourceChain,
+            protocolAddress,
+            abi.encode(orderId, state), // payload
+            0, // no receiver value needed since we're just passing a message
+            GAS_LIMIT
+        ); 
+        if(state==0){
+            IERC721(tokenAddress).safeTransferFrom(seller, buyer, tokenId);
+        }
+    }
 
-    function getListingIdCounter() public view returns(uint256){
-        return listingIdCounter;
-    }   
+    function quoteCrossChainCall(
+        uint16 targetChain, uint256 receiverValue
+    ) public view returns (uint256 cost) {
+        (cost, ) = wormholeRelayer.quoteEVMDeliveryPrice(
+            targetChain,
+            receiverValue,
+            GAS_LIMIT
+        );
+    }
 
 }
