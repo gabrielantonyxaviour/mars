95c199d969971a525d816baf2470d7aad1f1eac5
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-03-02 03:02:09 -0700

    list nft

--- frontend/src/components/NFTCard.tsx
+++ frontend/src/components/NFTCard.tsx
@@ -1,10 +1,26 @@
+import { nftContractAbi } from "@/utils/constants";
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { createPublicClient, getContract, http } from "viem";
+import {
+  arbitrumSepolia,
+  baseSepolia,
+  moonbaseAlpha,
+  polygonMumbai,
+  sepolia,
+} from "viem/chains";
 import { useAccount } from "wagmi";
 
+function resolveChain(chainId: string) {
+  if (chainId == "1287") return moonbaseAlpha;
+  else if (chainId == "80001") return polygonMumbai;
+  else if (chainId == "11155111") return sepolia;
+  else if (chainId == "84532") return baseSepolia;
+  else return arbitrumSepolia;
+}
 export default function NFTCard({
   image,
   chainId,
@@ -20,7 +36,25 @@ export default function NFTCard({
   mode: string;
   size: number;
 }) {
-  const { chain } = useAccount();
+  const publicClient = createPublicClient({
+    chain: resolveChain(chainId),
+    transport: http(),
+  });
+  const contract = getContract({
+    address: "0x620b89DeE45a3Fb1675182B8AD538B656b3D8366" as `0x${string}`,
+    abi: nftContractAbi,
+    client: publicClient,
+  });
+
+  useEffect(() => {
+    console.log(tokenId);
+    // (async function () {
+    //   const resOwnerOf = await contract.read.ownerOf([2]);
+    //   console.log(resOwnerOf);
+    //   const resTokenUri = await contract.read.tokenURI([2]);
+    //   console.log(resTokenUri);
+    // })();
+  }, []);
 
   return (
     <Link
